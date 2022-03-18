4fa7ea1df99834eb88e8672d55799e06c6b757dc
ccanabal <ccanabal@PC_CCANABAL>
2022-03-18 14:42:23 -0500

    Agregar archivos de proyecto.

---
+++ PruebaTecnica/Class1.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PruebaTecnica
+{
+    internal class Class1
+    {
+        public static void Main()
+        {
+            string path = File.ReadAllText("C:\\el_quijote.txt");
+
+            Console.WriteLine(path); // it ll display ur file on console
+
+            Console.Write(path.Length); // show character
+            Console.WriteLine();
+
+            Console.WriteLine(path.Split('\r').Length);  // total line
+
+            Console.WriteLine(path.Split(' ').Length); // total word
+
+
+        }
+
+    }
+}
